7d6737a534c6e15b3dc70bd19d06f168176d8ca2
leonsaja <nome do email>
2021-08-24 21:16:43 -0300

    app produto e  estoque criado

---
+++ estoque/models.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import User
+from django.db import models
+from core.models import DataCriacao
+from produto.models import Produto
+
+class Estoque(DataCriacao):
+
+    CHOICES_MOVIMENTO = (
+        ('E', 'Entrada'),
+        ('S', 'Saida'),
+    )
+
+    Funcionario=models.ForeignKey(User,on_delete=models.CASCADE, blank=True)
+    nf = models.PositiveIntegerField('Nota Fiscal',blank=True,null=True)
+    movimento=models.CharField(max_length=1,choices=CHOICES_MOVIMENTO,blank=True)
+
+
+class EstoqueItens(models.Model):
+
+    estoque = models.ForeignKey(Estoque, on_delete=models.CASCADE,related_name='estoques')
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    quantidade = models.PositiveIntegerField(blank=True, null=True)
+    saldo=models.PositiveIntegerField(default=0)
+
+    class meta:
+        ordering = ('pk',)
+
+    def __str__(self):
+        return '{} - {} - {}'.format(self.pk,self.estoque.pk, self.produto)
