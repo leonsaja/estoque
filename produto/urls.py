6b0660ae93c376a18a81116d927d821b1c70e7f9
leonsaja <nome do email>
2021-08-25 13:01:46 -0300

    detail produto

--- produto/urls.py
+++ produto/urls.py
@@ -4,6 +4,8 @@ from . import  views
 app_name = 'produto'
 urlpatterns = [
 
-    path('cadastar/produto', views.CadastrarProdutoView.as_view(), name='cadastrar_produto'),
-    path('listar/produtos', views.ListProdutosView.as_view(), name='listar_produtos'),
+    path('cadastar/produto/', views.CadastrarProdutoView.as_view(), name='cadastrar_produto'),
+    path('listar/produtos/', views.ListProdutosView.as_view(), name='listar_produtos'),
+    path('detail/produto/<int:pk>/', views.DetailProdutoView.as_view(), name='detail_produto'),
 ]
+
--- produto/views.py
+++ produto/views.py
@@ -1,12 +1,10 @@
 from django.db.models import Sum
 from django.shortcuts import render
-from django.views.generic import  CreateView,ListView
+from django.views.generic import CreateView, ListView, DetailView
 from produto.forms import CadastrarProdutoForm
 from produto.models import Produto
 
-
 class CadastrarProdutoView(CreateView):
-
     form_class =CadastrarProdutoForm
     template_name = 'produto/form_produto.html'
 
@@ -15,5 +13,14 @@ class ListProdutosView(ListView):
     context_object_name = 'produtos'
     template_name = 'produto/produto_list.html'
 
+class DetailProdutoView(DetailView):
+    model = Produto
+    template_name = 'produto/produto_detail.html'
+    context_object_name = 'produto'
 
-
+    #
+    # def get_context_data(self, **kwargs):
+    #     context=super(DetailProdutoView, self).get_context_data(**kwargs)
+    #     context['produto']=Produto.objects.get(pk=self.kwargs['pk'])
+    #
+    #     return context
