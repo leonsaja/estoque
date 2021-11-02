cd0603dc2cee669ce2f45f2a6e707e2fa8e8cfaf
leonsaja <nome do email>
2021-11-02 19:32:07 -0300

    correcao app estoque

--- estoque/urls.py
+++ estoque/urls.py
@@ -5,7 +5,7 @@ from estoque.views import estoque_itens_views
 app_name='estoque'
 
 urlpatterns = [
-    path('',TemplateView.as_view(template_name='estoque/index.html'),name='estoque_index'),
+    # path('',TemplateView.as_view(template_name='estoque/index.html'),name='estoque_index'),
     path('entrada/',estoque_views.EstoqueEntradaList.as_view(), name='estoque_entrada_list'),
     path('saida/',estoque_views.EstoqueSaidaList.as_view(), name='estoque_saida_list'),
     path('detalhe/entrada/<int:pk>/', estoque_views.EstoqueEntradadeteil.as_view(), name='estoque_entrada_detail'),
